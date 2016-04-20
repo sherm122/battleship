d601f4fc8760dcf4ad16e7cab5830d60728a77e8
czarn101 <czarn101@mail.chapman.edu>
2016-04-20 16:28:13 -0700

    game runs completely without error checks

--- Board.java
+++ Board.java
@@ -2,200 +2,303 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Board {
-    
-    public int row = 6;
-    public int col = 6;
-    public int shipSize = 3;
-    
-    public char[][] userChar2DArray = new char[row][col];
-    public char[][] compChar2DArray = new char[row][col];
-    public char[][] visCompChar2DArray = new char[row][col];
-    
-    public void initBoard(char[][] anyBoard){
-        
-        for(int i = 0; i < row; ++i){
-            
-            for(int j = 0; j < col; ++j){
-                anyBoard[i][j] = '~';
-                
-            }
-            
-        }
-        
-        
-    }
-    
-    public void printBoard(char[][] anyBoard){
-        
-        String textLine = " ";
-        
-        for(int i = 0; i < col; ++i){
-            textLine += " " + i;
-            
-        }
-        textLine += "\n";
-        for(int i = 0; i < row; ++i){
-            textLine += i;
-            for(int j = 0; j < col; ++j){
-                textLine += " " + anyBoard[i][j];
-                
-            }
-            textLine += "\n";
-        }
-        
-        System.out.println(textLine);
-    }
-    
-    public void setCompShips(char[][] board){
-        Random randShips = new Random();
-        
-        for (int shipSize = 1; shipSize <= 3; ++shipSize){
-            int startRow = 0;
-            int startCol = 0;
-            
-            boolean horizontal = randShips.nextBoolean();
-            
-            
-            if(horizontal){ //horizontal placement
-                boolean allow = false;
-                while(!allow){
-                    startRow = (int)(Math.random() * row);
-                    startCol = (int)(Math.random() * (col  - shipSize));
-                    allow = true;
-                    
-                    for(int i = 0; i < shipSize; ++i){
-                        if(board[startRow][startCol + i] != '~'){
-                            allow = false;
-                        }
-                    }
-                }
-                
-                for(int i = 0; i < shipSize; ++i){
-                    
-                    board[startRow][startCol + i] = 'O';
-                }
-            }
-            else{ //vertical placement
-                boolean allow = false;
-                while(!allow){
-                    startRow = (int)(Math.random() * (row - shipSize));
-                    startCol = (int)(Math.random() * col);
-                    allow = true;
-                    
-                    for(int i = 0; i < shipSize; ++i){
-                        if(board[startRow + i][startCol] != '~'){
-                            allow = false;
-                        }
-                    }
-                }
-                
-                for(int i = 0; i < shipSize; ++i){
-                    board[startRow + i][startCol] = 'O';
-                }
-            }
-        }
-    }
-    
-    
-    public void setUserShips(char[][]board, int shipSize){
-        Scanner input = new Scanner(System.in);
-        int inputRow;
-        int inputCol;
-        String inputHV;
-        for(int i = 1; i <= shipSize; ++i){
-            boolean allow = false;
-            while(!allow){
-                System.out.print("Type in an integer for row(shipSize: " + i +"): " );
-                inputRow = input.nextInt();
-                System.out.print("Type in an integer for column(shipSize: " + i +"): " );
-                inputCol = input.nextInt();
-                
-                System.out.println("Do you want to place the ship horizontally/vertically(h/v): ");
-                inputHV= input.next();
-                
-                if(inputHV.equals("h") || inputHV.equals("H")){
-                    int placeRow = inputRow;
-                    int placeCol = inputCol + i;
-                    if(placeCol >= col){
-                        allow = false;
-                        System.out.print("Coordinates are out of bounds");
-                        
-                    }
-                    else{
-                        allow = true;
-                        for(int j = 0; j < shipSize; ++j){
-                            if(board[placeRow][placeCol] != '~'){
-                                allow = false;
-                                System.out.print("Ship already exists in that location");
-                                
-                            }
-                            
-                        }
-                    }
-                    
-                    
-                }
-                
-                if(inputHV.equals("v") || inputHV.equals("V")){
-                    int placeRow = inputRow + i;
-                    int placeCol = inputCol;
-                    if(placeRow >= row){
-                        allow = false;
-                        System.out.print("Coordinates are out of bounds\n");
-                        
-                    }
-                    else{
-                        allow = true;
-                        
-                        for(int j = 0; j < shipSize; ++j){
-                            if(board[placeRow][placeCol] != '~'){
-                                allow = false;
-                                System.out.print("Ship already exists in that location\n");
-                            }
-                        }
-                    }
-                    
-                    
-                }	
-                
-                if(allow == true){
-                    if(inputHV.equals("v") || inputHV.equals("V")){
-                        for(int n = 0; n < i; ++n){
-                            board[inputRow+n][inputCol] = 'O';
-                        }
-                    }
-                    else if(inputHV.equals("h") || inputHV.equals("H")){
-                        for(int n = 0; n < i; ++n){
-                            board[inputRow][inputCol+n] = 'O';
-                        }
-                    }
-                    printBoard(board);
-                }
-                
-            }
-        }
-    }
-    
-    public static void main(String[] args) {
-        
-        
-        Board obj = new Board();
-        obj.initBoard(obj.userChar2DArray);
-        obj.initBoard(obj.compChar2DArray);
-        obj.initBoard(obj.visCompChar2DArray);
-        
-        System.out.println("This is the user board\n");
-        obj.printBoard(obj.userChar2DArray);
-        
-        //System.out.println("This is the computer board\n");
-        //obj.printBoard(obj.visCompChar2DArray);
-        
-        obj.setCompShips(obj.compChar2DArray);
-        //obj.printBoard(obj.compChar2DArray);
-        
-        obj.setUserShips(obj.userChar2DArray, obj.shipSize);
-        
-        
-    }
-    
+	
+	public int compShipCounter = 0;
+	public int userShipCounter = 0;
+	public int row = 6;
+	public int col = 6;
+	public int shipSize = 3;
+	
+	public char[][] userChar2DArray = new char[row][col];
+	public char[][] compChar2DArray = new char[row][col];
+	public char[][] visCompChar2DArray = new char[row][col];
+	
+	public void initBoard(char[][] anyBoard){
+		
+		for(int i = 0; i < row; ++i){
+			
+			for(int j = 0; j < col; ++j){
+				anyBoard[i][j] = '~';
+				
+			}
+			
+		}
+		
+		
+	}
+	
+	public void printBoard(char[][] anyBoard){
+		
+		String textLine = " ";
+		
+		for(int i = 0; i < col; ++i){
+			textLine += " " + i;
+		
+		}
+		textLine += "\n";
+		for(int i = 0; i < row; ++i){
+			textLine += i;
+			for(int j = 0; j < col; ++j){
+				textLine += " " + anyBoard[i][j];
+				
+			}
+			textLine += "\n";
+		}
+		
+		System.out.println(textLine);
+	}
+	
+	public void setCompShips(char[][] board){
+		Random randShips = new Random();
+
+		for (int shipSize = 1; shipSize <= 3; ++shipSize){	
+			int startRow = 0;
+			int startCol = 0;
+			
+			boolean horizontal = randShips.nextBoolean();
+			
+			
+			if(horizontal){ //horizontal placement
+				boolean allow = false;
+				while(!allow){
+					startRow = (int)(Math.random() * row);
+					startCol = (int)(Math.random() * (col  - shipSize));
+					allow = true;
+					
+					for(int i = 0; i < shipSize; ++i){
+						if(board[startRow][startCol + i] != '~'){
+							allow = false;
+						}
+					}
+				}
+				
+				for(int i = 0; i < shipSize; ++i){
+					
+					board[startRow][startCol + i] = 'O';
+				}
+			}
+			else{ //vertical placement
+				boolean allow = false;
+				while(!allow){
+					startRow = (int)(Math.random() * (row - shipSize));
+					startCol = (int)(Math.random() * col);
+					allow = true;
+					
+					for(int i = 0; i < shipSize; ++i){
+						if(board[startRow + i][startCol] != '~'){
+							allow = false;
+						}
+					}
+				}
+				
+				for(int i = 0; i < shipSize; ++i){
+					board[startRow + i][startCol] = 'O';
+				}
+			}
+		}
+	}
+		
+	
+	public void setUserShips(char[][]board, int shipSize){
+		Scanner input = new Scanner(System.in);
+		int inputRow; 
+		int inputCol;
+		String inputHV; 
+		for(int i = 1; i <= shipSize; ++i){
+			boolean allow = false;
+			while(!allow){
+				System.out.print("Type in an integer for row(shipSize: " + i +"): " );
+				inputRow = input.nextInt();
+				System.out.print("Type in an integer for column(shipSize: " + i +"): " );
+				inputCol = input.nextInt();
+				
+				System.out.print("Do you want to place the ship horizontally/vertically(h/v): ");
+				inputHV= input.next();
+				System.out.println();
+				
+				if(inputHV.equals("h") || inputHV.equals("H")){
+						int placeRow = inputRow;
+						int placeCol = inputCol + i - 1;
+						if(placeCol >= col){
+							allow = false;
+							System.out.println("Coordinates are out of bounds");
+							
+						}
+						else{
+							allow = true;
+							if((board[placeRow][placeCol] != '~') ){ //|| (board[placeRow][(placeCol -1)] != '~') if theres an issue use this
+								System.out.println("Ship already exists in that location");
+								allow = false;
+							}
+						}		
+				}
+				
+				if(inputHV.equals("v") || inputHV.equals("V")){
+						int placeRow = inputRow + i - 1; //+ (i-1);
+						int placeCol = inputCol;
+						if(placeRow >= row){
+							allow = false;
+							System.out.println("Coordinates are out of bounds");
+						
+						}
+						else{
+							allow = true;
+							if((board[placeRow][placeCol] != '~') ){ // || (board[(placeRow-1)][placeCol] != '~') if theres an issue use this
+								System.out.println("Ship already exists in that location");
+								allow = false;
+							}
+						}
+				}	
+				
+				if(allow == true){
+					if(inputHV.equals("v") || inputHV.equals("V")){
+						for(int n = 0; n < i; ++n){
+							board[inputRow+n][inputCol] = 'O';
+						}
+					}
+					else if(inputHV.equals("h") || inputHV.equals("H")){
+						for(int n = 0; n < i; ++n){
+							board[inputRow][inputCol+n] = 'O';
+						}
+					}
+					printBoard(board);
+				}
+				
+			}
+		}
+	}
+	
+	public void userFire(char[][]compChar2DArray){
+		Scanner input = new Scanner(System.in);
+		boolean allow = false;
+		
+		while(!allow){
+			System.out.print("Input the row you want to hit: ");
+			int inputRow = input.nextInt();
+			
+			System.out.print("Input the column you want to hit: ");
+			int inputCol = input.nextInt();
+		
+			if((inputRow < 0 || inputRow > row) || (inputCol < 0 || inputCol > col)){ //checks if the coordinates are out of bounds
+				System.out.println("You are firing out of bounds, try again: ");
+				allow = false;
+			}
+			else{ //the coordinates are in bounds
+				allow = true;
+				
+				if((compChar2DArray[inputRow][inputCol] == 'X') || (compChar2DArray[inputRow][inputCol] == '*')){
+					System.out.println("You have already used this coordinate shoot again");
+					allow = false;
+				}
+				else if(compChar2DArray[inputRow][inputCol] == 'O'){ //if it hits a ship
+					System.out.println("You hit a ship!!");
+					compChar2DArray[inputRow][inputCol] = 'X';
+					visCompChar2DArray[inputRow][inputCol] = 'X';
+					//printBoard(compChar2DArray); //eventually comment this out
+					printBoard(visCompChar2DArray);
+					userShipCounter++;
+					if(userShipCounter == 6){
+						System.out.println("You win, computer loses!!");
+						System.exit(0);
+								
+					}
+				}
+				else{ //it is a miss
+					System.out.println("You have missed");
+					visCompChar2DArray[inputRow][inputCol] = '*';
+					compChar2DArray[inputRow][inputCol] = '*';
+					printBoard(visCompChar2DArray);
+							
+				}
+
+			
+			}
+		}
+	}
+	
+	public void compFire(char[][] userChar2DArray){
+		
+		Random rand = new Random();
+		boolean active = false;
+		int compRow;
+		int compCol;
+		
+		
+		while(!active){
+			compRow = rand.nextInt(row);
+			compCol = rand.nextInt(col);
+			active = true;
+			if((userChar2DArray[compRow][compCol] == 'X') || (userChar2DArray[compRow][compCol] == '*')){
+				active = false;
+				
+			}
+			
+			else if(userChar2DArray[compRow][compCol] == 'O'){ //if it hits a ship
+				System.out.println("Computer hit a ship!!");
+				userChar2DArray[compRow][compCol] = 'X';
+				printBoard(userChar2DArray);
+				compShipCounter++;
+				if(compShipCounter == 6){
+					System.out.println("Computer won, you lose!!");
+					System.exit(0);
+					
+				}
+					
+			}
+			else{ //it is a miss
+				System.out.println("Computer has missed");
+				userChar2DArray[compRow][compCol] = '*';
+				printBoard(userChar2DArray);
+						
+			}
+			
+			
+		}
+		/*for(int i = 0; i < row; ++i){
+			for(int j = 0; j < col; ++j){
+				if(userChar2DArray[i][j] == 'X'){ //if there are no more ships left checking each row and col
+					shipCounter++;
+									
+				}
+				if(shipCounter == 6){
+					System.out.println("Computer wins!!");
+					System.exit(0);
+							
+				}
+			}
+		}*/
+		
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Board obj = new Board();
+		obj.initBoard(obj.userChar2DArray);
+		obj.initBoard(obj.compChar2DArray);
+		obj.initBoard(obj.visCompChar2DArray);
+		
+		System.out.println("This is the user board\n");
+		obj.printBoard(obj.userChar2DArray);
+		
+		//System.out.println("This is the computer board\n");
+		//obj.printBoard(obj.visCompChar2DArray);
+		
+		obj.setCompShips(obj.compChar2DArray);
+		//obj.printBoard(obj.compChar2DArray);
+		
+		obj.setUserShips(obj.userChar2DArray, obj.shipSize);
+		
+		while(true){
+			obj.userFire(obj.compChar2DArray);
+			obj.compFire(obj.userChar2DArray);
+			
+		}
+		
+		
+
+	}
+
 }
